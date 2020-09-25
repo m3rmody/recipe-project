85da9934e6b588a90708bc7ae12908fcbd19886c
m3rmody <dmytro.shevchenkoit@gmail.com>
2020-09-25 19:58:49 +0200

    Added data loader and add optional in repositories and check is evety thing is working in IndexController.

--- src/main/java/mermody/springframework/recipeproject/controllers/IndexController.java
+++ src/main/java/mermody/springframework/recipeproject/controllers/IndexController.java
@@ -1,13 +1,33 @@
 package mermody.springframework.recipeproject.controllers;
 
+import mermody.springframework.recipeproject.domain.Category;
+import mermody.springframework.recipeproject.domain.UnitOfMeasure;
+import mermody.springframework.recipeproject.repositories.CategoryRepository;
+import mermody.springframework.recipeproject.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private final CategoryRepository categoryRepository;
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"","/","index"})
     public String getIndexPage(){
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Cat Id  is: "+categoryOptional.get().getId());
+        System.out.println("UOF Id  is: "+unitOfMeasureOptional.get().getId());
+
         return "index";
     }
 }
--- src/main/java/mermody/springframework/recipeproject/repositories/UnitOfMeasureRepository.java
+++ src/main/java/mermody/springframework/recipeproject/repositories/UnitOfMeasureRepository.java
@@ -1,7 +1,12 @@
 package mermody.springframework.recipeproject.repositories;
 
+import mermody.springframework.recipeproject.domain.Category;
 import mermody.springframework.recipeproject.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
